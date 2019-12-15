26dedb0cea23d6c909481ca72eac481ba5977eb2
Ttzombor <ttzombor@gmail.com>
2019-12-15 20:38:04 +0200

    Finished with task. Added new options: Zero Comments(CheckBox), Number in the top and Numbers of Comments

--- wp-includes/widgets/class-wp-widget.php
+++ wp-includes/widgets/class-wp-widget.php
@@ -7,10 +7,10 @@ class WP_My_Widget extends WP_Widget {
 */
 public function __construct() {
 $widget_ops = array(
-'classname' => 'my_widget',
-'description' => 'My Widget is awesome',
+'classname' => 'Top users',
+'description' => 'Top users by comments',
 );
-parent::__construct( 'my_widget', 'My Widget', $widget_ops );
+parent::__construct( 'my_widget', 'Top users', $widget_ops );
 }
 
 /**
@@ -20,7 +20,62 @@ parent::__construct( 'my_widget', 'My Widget', $widget_ops );
 * @param array $instance
 */
 public function widget( $args, $instance ) {
-// outputs the content of the widget
+    $title = apply_filters( 'widget_title', $instance[ 'title' ] );
+
+    echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title'];
+
+    $user_array = array();
+    $users = get_users();
+
+    $number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
+		if ( ! $number ) {
+			$number = 5;
+		}
+
+		$zero_comments = $instance['zero_comments'];
+    $id = 0;
+    foreach ($users as $user){
+        $args = array(
+               'user_id' => $user->ID
+        );
+        $comments = get_comments( $args );
+
+        if(!$zero_comments && !$comments)  continue;
+        $i = 0;
+        $user_array[$id]['user_id'] = $user->ID;
+        $user_array[$id]['user_name'] = $user->user_login;
+        $user_array[$id]['number_of_comments'] =$i;
+            foreach ($comments as $comment) {
+
+                $user_array[$id]['comment_content'][$i] = $comment->comment_content;
+                $user_array[$id]['number_of_comments'] = ++$i;
+
+            }
+        ++$id;
+            if($id == $number) break;
+    }
+    arsort($user_array);
+?>
+    <ol>
+    <?php
+    $i = 1;
+    foreach($user_array as $item){
+        ?>
+            <li ><?php
+                if($instance['number_of_comments'])
+                    echo $i .". ". $item['user_name'] . " (" . $item['number_of_comments'] . ")";
+                else
+                    echo $i .". ". $item['user_name'] ;
+                ?></li>
+
+        <?php
+        ++$i;
+    }
+
+    ?>
+    </ol>
+<?php
+    echo $args['after_widget'];
 }
 
 /**
@@ -29,7 +84,29 @@ public function widget( $args, $instance ) {
 * @param array $instance The widget options
 */
 public function form( $instance ) {
-// outputs the options form on admin
+    $title = ! empty( $instance['title'] ) ? $instance['title'] : '';
+    $number_of_comments = isset( $instance['number_of_comments'] ) ? $instance['number_of_comments'] : true;
+    $number = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+    $zero_comments = isset( $instance['zero_comments'] ) ? $instance['zero_comments'] : true ?>
+
+    <p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+    <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+    <label for="<?php echo $this->get_field_id( 'number_of_comments' ); ?>">Number of comments:</label>
+    <input class="checkbox" type="checkbox" <?php checked( $instance[ 'number_of_comments' ], 'on' ); ?> id="<?php echo
+    $this->get_field_id( 'number_of_comments' ); ?>" name="<?php echo $this->get_field_name( 'number_of_comments' ); ?>" />
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php echo(
+                'Number of users :' ); ?></label>
+		<input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" /></p>
+    <p>
+    <label for="<?php echo $this->get_field_id( 'zero_comments' ); ?>">Zero comments users:</label>
+        <input class="checkbox" type="checkbox" <?php checked( $instance[ 'zero_comments' ], 'on' ); ?> id="<?php echo
+        $this->get_field_id( 'zero_comments' ); ?>" name="<?php echo $this->get_field_name( 'zero_comments' ); ?>" />
+    </p>
+    <?php
 }
 
 /**
@@ -41,6 +118,11 @@ public function form( $instance ) {
 * @return array
 */
 public function update( $new_instance, $old_instance ) {
-// processes widget options to be saved
+    $instance = $old_instance;
+    $instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+    $instance['number_of_comments'] = $new_instance['number_of_comments'];
+    $instance['number'] = absint( $new_instance['number'] );
+    $instance['zero_comments'] = $new_instance['zero_comments'];
+    return $instance;
 }
 }

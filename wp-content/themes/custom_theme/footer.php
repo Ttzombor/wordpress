4574d96866cc8f19d4c767ae8816fe5a7dd9b0fe
Ttzombor <ttzombor@gmail.com>
2020-01-13 11:38:26 +0200

    Developed something

--- wp-content/themes/custom_theme/footer.php
+++ wp-content/themes/custom_theme/footer.php
@@ -1,5 +1,21 @@
 
 <footer class="site-footer">
+    <?php if( get_theme_mod('custom_footer_to_display_callout') == 'Yes'){ ?>
+    <div class="additional-footer-section">
+            <div class="additional-footer-section-image">
+               <a href="<?php echo get_permalink((get_theme_mod('custom_footer_link_callout'))); ?>"><img src ="<?php echo wp_get_attachment_url(get_theme_mod('custom_footer_image_callout')); ?>"></a>
+            </div>
+        <div class="additional-footer-section-text">
+            <h2>
+                <?php echo get_theme_mod('custom_footer_header_callout'); ?>
+            </h2>
+
+            <?php echo wpautop(get_theme_mod('custom_footer_text_callout')); ?>
+            <a href="<?php echo get_permalink(get_theme_mod('custom_footer_link_callout')) ?>"><p>Show more &raquo</p></a>
+        </div>
+
+    </div>
+    <?php } ?>
 
     <div class="footer-widget">
 
--- wp-content/themes/custom_theme/page.php
+++ wp-content/themes/custom_theme/page.php
@@ -1,57 +1,42 @@
 <?php
 
 get_header();
+?>
+    <div class="site-content clearfix">
+    <div class="main-column">
+<?php        if (have_posts()) :
 
-if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
-        <article class="post page">
-                <nav class="site-nav children-links clearfix">
-                    <span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id()); ?>"><?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
-                    <ul>
-                        <?php
-
-                        $args = array(
-                            'child_of' => get_top_ancestor_id(),
-                            'title_li' => ''
-                        );
-
-                        ?>
-                        <?php wp_list_pages($args); ?>
-                    </ul>
-                </nav>
-
-            <?php the_post_thumbnail('banner-thumbnails') ?>
-
-            <h2><?php the_title(); ?></h2>
-            <?php the_content(); ?>
-        </article>
-    <?php endwhile;
-
-else :
-    echo '<p>No content found</p>';
-
-endif;?>
-<?php
-    $recentComments = new WP_Query(array(
-            'post_type' => 'post',
-            'posts_per_page' => 2,
-            'orderby' => 'rand'
-    ));
-if ($recentComments->have_posts()) :
-    while ($recentComments->have_posts()) : $recentComments->the_post();
+        while (have_posts()) : the_post();
         $uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
         if(get_the_title() != ucfirst($uriSegments[2])) {
-            get_template_part('template-parts/content', get_post_format());
+        get_template_part('template-parts/content', get_post_format());
         }
-    endwhile;
+        endwhile;
 
 else :
     echo '<p>No content found</p>';
 
 endif;
 
-    wp_reset_postdata();
-?>
+/*$recentComments = new WP_Query(array(
+    'post_type' => 'post',
+    'posts_per_page' => 2,
+    'orderby' => 'rand'
+));
+if ($recentComments->have_posts()) :
+    while ($recentComments->have_posts()) : $recentComments->the_post();
+        $uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        if (get_the_title() != ucfirst($uriSegments[2])) {
+            get_template_part('template-parts/content', get_post_format());
+        }
+    endwhile;
+endif;
+wp_reset_postdata();
+
+*/?>
+    </div>
+    <?php get_sidebar(); ?>
+    </div>
 <?php
 get_footer();
 ?>
